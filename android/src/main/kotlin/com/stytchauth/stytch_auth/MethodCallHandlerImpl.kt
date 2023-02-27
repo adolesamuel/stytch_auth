92e50c313594131d096d7f6e6c9761e969bbe618
Adole Samuel Sani <adolesamuel4@gmail.com>
2023-02-27 21:11:51 +0100

    fix four

--- android/src/main/kotlin/com/stytchauth/stytch_auth/MethodCallHandlerImpl.kt
+++ android/src/main/kotlin/com/stytchauth/stytch_auth/MethodCallHandlerImpl.kt
@@ -1,7 +1,9 @@
-import com.stytch.sdk.StytchClient
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.util.Log
+import com.stytch.sdk.consumer.StytchClient
+import com.stytch.sdk.consumer.oauth.OAuth
 import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -11,8 +13,8 @@ import org.json.JSONObject
 
 
 internal class MethodCallHandlerImpl(context: Context, activity: Activity?, eventChannel: EventChannel, methodChannel: MethodChannel
-): MethodCallHandler, EventChannel.StreamHandler, LoginListener,BoolListener, FetchUserListener{
-    private var context: Context?
+): MethodCallHandler, EventChannel.StreamHandler{
+    private var context: Context
     private var activity: Activity?
     private var events: EventChannel.EventSink? = null
     private var eventChannel: EventChannel
@@ -26,6 +28,13 @@ internal class MethodCallHandlerImpl(context: Context, activity: Activity?, even
         this.eventChannel = eventChannel
         this.methodChannel = methodChannel
         eventChannel.setStreamHandler(this)
+    }
+
+    override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+        this.events = events
+    }
+
+    override fun onCancel(arguments: Any?) {
     }
 
     fun setActivity(act: Activity?) {
@@ -54,39 +63,45 @@ internal class MethodCallHandlerImpl(context: Context, activity: Activity?, even
     private fun configure(call: MethodCall){
         val publicToken = call.argument<String>("public_token")
         StytchClient.configure(
-                context = context,
-                publicToken = publicToken
+              context =  context,
+               publicToken = publicToken!!
         )
     }
 
-    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        when (requestCode) {
-            [YOUR OAUTH_REQUEST_CODE] -> data?.let { viewModel.oauthAuthenticate(resultCode, it) }
-        }
-    }
+//    public  fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+////        super.onActivityResult(requestCode, resultCode, data)
+//        when (requestCode) {
+//            3 -> data?.let { viewModel.oauthAuthenticate(resultCode, it) }
+//        }
+//    }
 
-    fun oauthAuthenticate(resultCode: Int, intent: Intent) {
-        viewModelScope.launch {
-            if (resultCode == RESULT_OK) {
-                intent.data?.let {
-                    val result = StytchClient.handle(it, 60U)
-                    // result is either a successfully authenticated session, or an error message
-                }
-            } else {
-                intent.extras?.getSerializable(OAuthError.OAUTH_EXCEPTION)?.let {
-                    when (it as OAuthError) {
-                        is OAuthError.UserCanceled -> {} // do nothing
-                        is OAuthError.NoBrowserFound,
-                        is OAuthError.NoURIFound -> // provide appropriate messaging to the user
-                    }
-                }
-            }
-        }
-    }
+//    fun oauthAuthenticate(resultCode: Int, intent: Intent) {
+//        viewModelScope.launch {
+//            if (resultCode == RESULT_OK) {
+//                intent.data?.let {
+//                    val result = StytchClient.handle(it, 60U)
+//                    // result is either a successfully authenticated session, or an error message
+//                }
+//            } else {
+//                intent.extras?.getSerializable(OAuthError.OAUTH_EXCEPTION)?.let {
+//                    when (it as OAuthError) {
+//                        is OAuthError.UserCanceled -> {} // do nothing
+//                        is OAuthError.NoBrowserFound,
+//                        is OAuthError.NoURIFound -> // provide appropriate messaging to the user
+//                    }
+//                }
+//            }
+//        }
+//    }
 
     private fun startGoogleLogin(){
-        StytchClient.oauth.google.start(parameters: OAuth.ThirdParty.StartParameters)
+        val startParameters = OAuth.ThirdParty.StartParameters(
+                context = activity!!,
+                oAuthRequestIdentifier = 3,
+                loginRedirectUrl = "app://oauth",
+                signupRedirectUrl = "app://oauth",
+        )
+        StytchClient.oauth.google.start(startParameters)
     }
 
 
--- android/src/main/kotlin/com/stytchauth/stytch_auth/StytchAuthPlugin.kt
+++ android/src/main/kotlin/com/stytchauth/stytch_auth/StytchAuthPlugin.kt
@@ -1,9 +1,11 @@
 package com.stytchauth.stytch_auth
 
+import MethodCallHandlerImpl
 import androidx.annotation.NonNull
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import io.flutter.plugin.common.EventChannel
 import org.json.JSONArray
@@ -11,18 +13,21 @@ import org.json.JSONObject
 
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** StytchAuthPlugin */
-class StytchAuthPlugin: FlutterPlugin, MethodCallHandler {
+class StytchAuthPlugin: FlutterPlugin, ActivityAware {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
+  private lateinit var eventChannel: EventChannel
   private var handler: MethodCallHandlerImpl? = null
   private val channelId = "stytch_auth"
   private val eventChannelId = "stytch_auth/event"
@@ -30,11 +35,12 @@ class StytchAuthPlugin: FlutterPlugin, MethodCallHandler {
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, channelId)
-    eventChannel = EventChannel(binding.binaryMessenger, eventChannelId)
+    eventChannel = EventChannel(flutterPluginBinding.binaryMessenger, eventChannelId)
     handler = MethodCallHandlerImpl(flutterPluginBinding.applicationContext, null, eventChannel, channel)
 
     channel.setMethodCallHandler(handler)
   }
+
 
   // override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
   //   if (call.method == "getPlatformVersion") {
@@ -44,30 +50,31 @@ class StytchAuthPlugin: FlutterPlugin, MethodCallHandler {
   //   }
   // }
 
-  // override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-  //   channel.setMethodCallHandler(null)
-  // }
+   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+     channel.setMethodCallHandler(null)
+   }
 
-  // override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-  //   handler?.setActivity(binding.activity)
-  // }
 
-  // public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-  //   super.onActivityResult(requestCode, resultCode, data)
-  //   when (requestCode) {
-  //     [YOUR OAUTH_REQUEST_CODE] -> data?.let { viewModel.oauthAuthenticate(resultCode, it) }
-  //   }
-  // }
+   override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+     handler?.setActivity(binding.activity)
+   }
 
-  // override fun onDetachedFromActivityForConfigChanges() {
-  //   handler?.setActivity(null)
-  // }
+//   public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//     super.onActivityResult(requestCode, resultCode, data)
+//     when (requestCode) {
+//       [YOUR OAUTH_REQUEST_CODE] -> data?.let { viewModel.oauthAuthenticate(resultCode, it) }
+//     }
+//   }
 
-  // override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-  //   onAttachedToActivity(binding)
-  // }
+   override fun onDetachedFromActivityForConfigChanges() {
+     handler?.setActivity(null)
+   }
 
-  // override fun onDetachedFromActivity() {
-  //   ///
-  // }
+   override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+     onAttachedToActivity(binding)
+   }
+
+   override fun onDetachedFromActivity() {
+     ///
+   }
 }
