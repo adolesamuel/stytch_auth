76ec92966a3cbd3521ee116dfc02f1b9480266d8
Adole Samuel Sani <adolesamuel4@gmail.com>
2023-02-27 14:51:05 +0100

    fixes two

---
+++ lib/src/src.dart
@@ -0,0 +1,3 @@
+export 'stytch_auth.dart';
+export 'stytch_auth_flutter_method_channel.dart';
+export 'stytch_auth_flutter_platform_interface.dart';
--- lib/src/stytch_auth.dart
+++ lib/src/stytch_auth.dart
@@ -1,5 +1,7 @@
 library stytch_auth;
 
+import 'package:stytch_auth/src/stytch_auth_flutter_platform_interface.dart';
+
 class StytchAuthFlutter {
   /// Private constructor for only one instance of stytch auth to exists.
   ///
@@ -9,4 +11,15 @@ class StytchAuthFlutter {
 
   /// Get the instance of [StytchAuthFlutter].
   static StytchAuthFlutter get instance => _instance;
+
+  Future<void> configure({
+    required String publicToken,
+  }) {
+    return StytchAuthFlutterPlatform.instance
+        .configure(publicToken: publicToken);
+  }
+
+  Future<void> loginWithGoogle() {
+    return StytchAuthFlutterPlatform.instance.loginWithGoogle();
+  }
 }
